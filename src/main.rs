03e71984a579a4c6717c4b0520c1a44a7ed02796
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-02-28 00:41:04 +0200

    *

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use crossterm;
 use crossterm::cursor;
 use crossterm::event;
 use crossterm::event::read;
+use crossterm::style;
+use crossterm::style::style;
 use crossterm::terminal;
 use crossterm::ExecutableCommand;
 use crossterm::QueueableCommand;
+use std::io::Stdout;
 use std::io::Write;
 use terminal::EnterAlternateScreen;
 use terminal::LeaveAlternateScreen;
@@ -15,6 +18,7 @@ enum Action {
     MoveLeft,
     MoveRight,
     Quit,
+    EnterMode(Mode),
 }
 
 enum Mode {
@@ -22,28 +26,45 @@ enum Mode {
     Insert,
 }
 
-fn handle_mode(mode: &Mode, event: event::Event) -> anyhow::Result<Option<Action>> {
+fn handle_mode(stdout: &mut Stdout , mode: &Mode, event: event::Event) -> anyhow::Result<Option<Action>> {
     match mode {
-        Mode::Insert => handle_insert_mode(event),
-        Mode::Normal => handle_normal_mode(event),
+        Mode::Insert => handle_insert_mode(stdout, event),
+        Mode::Normal => handle_normal_mode(stdout, event),
     }
 }
 
-fn handle_normal_mode(event: event::Event) -> anyhow::Result<Option<Action>> {
+fn handle_normal_mode(stdout: &mut Stdout, event: event::Event) -> anyhow::Result<Option<Action>> {
     match event {
-        event::Event::Key(event) => match event.code {
+        event::Event::Key(_event) => match _event.code {
+            // exit (aka. quit)
             event::KeyCode::Char('q') => Ok(Some(Action::Quit)),
+            // Movement
             event::KeyCode::Char('h') => Ok(Some(Action::MoveLeft)),
+            event::KeyCode::Left => Ok(Some(Action::MoveLeft)),
             event::KeyCode::Char('j') => Ok(Some(Action::MoveDown)),
+            event::KeyCode::Down => Ok(Some(Action::MoveDown)),
             event::KeyCode::Char('k') => Ok(Some(Action::MoveUp)),
+            event::KeyCode::Up => Ok(Some(Action::MoveUp)),
             event::KeyCode::Char('l') => Ok(Some(Action::MoveRight)),
+            event::KeyCode::Right => Ok(Some(Action::MoveRight)),
+            event::KeyCode::Char('i') => Ok(Some(Action::EnterMode(Mode::Insert))),
             _ => Ok(None),
         },
         _ => Ok(None),
     }
 }
-fn handle_insert_mode(event: event::Event) -> anyhow::Result<Option<Action>> {
-    unimplemented!("insert mode: {:?}", event)
+fn handle_insert_mode(stdout: &mut Stdout, event: event::Event) -> anyhow::Result<Option<Action>> {
+    match event {
+     event::Event::Key(_event) => match _event.code {
+        event::KeyCode::Esc => Ok(Some(Action::EnterMode(Mode::Normal))),
+        event::KeyCode::Char(character) => {
+            stdout.queue(style::Print(character))?;
+            Ok(None)
+        }
+        _ => Ok(None),
+     },
+        _ => Ok(None),
+    }
 }
 fn main() -> anyhow::Result<()> {
     let mut stdout = std::io::stdout();
@@ -63,7 +84,7 @@ fn main() -> anyhow::Result<()> {
         stdout.queue(cursor::MoveTo(cursor_x, cursor_y))?;
         stdout.flush()?;
         let event = read()?;
-        if let Some(action) = handle_mode(&mode, event)? {
+        if let Some(action) = handle_mode(&mut stdout, &mode, event)? {
             match action {
                 Action::Quit => break,
                 Action::MoveDown => {
@@ -77,7 +98,10 @@ fn main() -> anyhow::Result<()> {
                 }
                 Action::MoveLeft => {
                     cursor_x = cursor_x.saturating_sub(1);
-                }
+                },
+                Action::EnterMode(new_mode) => {
+                    mode = new_mode;
+                },
                 _ => {}
             }
         }
